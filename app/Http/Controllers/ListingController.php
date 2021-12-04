4e7f7e10995ac1dc0de149f7507af739ce9a7b59
SBTHDR <subrata.halder13d@gmail.com>
2021-12-04 19:40:30 +0600

    Layout components

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -14,7 +14,12 @@ class ListingController extends Controller
      */
     public function index()
     {
-        //
+        $listings = Listing::where('is_active', true)
+            ->with('tags')
+            ->latest()
+            ->get();
+
+        return view('listings.index', compact('listings'));
     }
 
     /**
---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,8 @@
+<section class="text-gray-600 body-font">
+    <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+      <div class="text-center lg:w-2/3 w-full">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">DataBlock - Data Science Job Listing</h1>
+        <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>        
+      </div>
+    </div>
+  </section>

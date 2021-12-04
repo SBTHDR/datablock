11eec3800edc106e24dd0970df3addaf451f7c54
SBTHDR <subrata.halder13d@gmail.com>
2021-12-04 14:32:17 +0600

    Factory adn Seeder

--- database/migrations/2021_12_04_124510_create_listings_table.php
+++ database/migrations/2021_12_04_124510_create_listings_table.php
@@ -16,7 +16,7 @@ class CreateListingsTable extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('name');
+            $table->string('title');
             $table->string('slug');
             $table->string('company');
             $table->string('location');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $tags = Tag::factory(10)->create();
+
+        User::factory(20)->create()->each(function ($user) use ($tags) {
+            Listing::factory(rand(1, 4))->create([
+                'user_id' => $user->id
+            ])->each(function ($listing) use ($tags) {
+                $listing->tags()->attach($tags->random(2));
+            });
+        });
     }
 }
